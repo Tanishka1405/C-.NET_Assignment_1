4d8748bff225e50370faff69099f8bf023ef90f2
Tanishka1405 <tanishka14004@gmail.com>
2024-02-12 10:37:05 -0500

    fourth commit

--- Assignment2/Interval_assign_2.cs
+++ Assignment2/Interval_assign_2.cs
@@ -9,20 +9,20 @@ class Program
         double x = double.Parse(Console.ReadLine());
 
         // Define the intervals
-        double interval1Start = 2;
-        double interval1End = 3;
-        double interval2Start = 0;
-        double interval2End = 1;
-        double interval3Start = -10;
-        double interval3End = -2;
+        double interval1a = 2;
+        double interval1b = 3;
+        double interval2a = 0;
+        double interval2b = 1;
+        double interval3a = -10;
+        double interval3b = -2;
 
         // Test membership using only < and ==
-        bool condition1 = (x >= interval1Start) && (x < interval1End);
-        bool condition2 = (x > interval2Start) && (x <= interval2End);
-        bool condition3 = (x >= interval3Start) && (x <= interval3End);
+        bool set1 = (x >= interval1a) && (x < interval1b);
+        bool set2 = (x > interval2a) && (x <= interval2b);
+        bool set3 = (x >= interval3a) && (x <= interval3b);
 
         // Display the result based on logical operators
-        if (((condition1 || condition2) || (condition3)))
+        if (((set1 || set2) || (set3)))
         {
             Console.WriteLine("x belongs to I");
         }
